9b3584c282052f798d1dcd04bf9a8dd9decfa76c
angelop47 <pescettoangelo@gmail.com>
2025-10-21 19:49:52 -0300

    Refactor project structure: update Nodemon configuration to execute server.ts, add Express and CORS dependencies, and implement basic server setup with configuration management using dotenv.

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { type Application, type Request, type Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(cors()); 
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Servidor funcionando correctamente');
+});
+
+export default app;
